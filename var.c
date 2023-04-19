d86fbdebb884b8ca38299b80b5cefc40ace50b1a
pavandave <116074733+thetoy0319@users.noreply.github.com>
2023-04-19 19:25:52 +0530

    Add files via upload

---
+++ var.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main (){
+    int a=10;
+    int b=5;
+    int sum;
+
+sum = a + b;
+printf(" sum is a+b %d\n ",sum);
+
+sum = a - b;
+printf(" sum is a+b %d\n ",sum);
+
+sum = a * b;
+printf(" sum is a+b %d\n ",sum);
+
+sum = a / b;
+printf(" sum is a+b %d\n ",sum);
+sum = a % b;
+printf(" sum is a+b %d\n ",sum);
+
+
+    return 0;
+}
